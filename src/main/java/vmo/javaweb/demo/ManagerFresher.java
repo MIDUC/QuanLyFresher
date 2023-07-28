4f078a615db340ba79ca50e6b92320ae0a43884f
Minhduc29601 <duc12534@gmail.com>
2023-07-28 16:49:06 +0700

    Swagger

--- src/main/java/vmo/javaweb/demo/ManagerFresher.java
+++ src/main/java/vmo/javaweb/demo/ManagerFresher.java
@@ -2,9 +2,13 @@ package vmo.javaweb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class ManagerFresher {
+@EnableWebMvc
+public class ManagerFresher{
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerFresher.class, args);
